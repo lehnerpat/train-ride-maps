80c381e4704b41a2b6953d1286bcd9dd7764cc00
Patrick Lehner <lehner.pa@gmail.com>
2022-02-25 21:22:10 +0100

    Add another example route (rough routing done)

--- src/included-data/index.ts
+++ src/included-data/index.ts
@@ -1,4 +1,5 @@
 import { Route } from "../route-models";
+import v20200110_Nishimaizuru_Fukuchiyama from "./2020-01-10_nishimaizuru-fukuchiyama.json";
 import v20210319_Hiroshima_Miyoshi from "./2021-03-19_hiroshima-miyoshi.json";
 
-export const IncludedData: Route[] = [v20210319_Hiroshima_Miyoshi];
+export const IncludedData: Route[] = [v20200110_Nishimaizuru_Fukuchiyama, v20210319_Hiroshima_Miyoshi];
