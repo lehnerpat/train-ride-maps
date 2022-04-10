c4e4cc0fd1a38dda73a599a0442ee00e30cdf0b9
Patrick Lehner <lehner.pa@gmail.com>
2022-04-10 18:24:05 +0200

    Remove v1 ViewTrackPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import { FC } from "react";
-import { TrackPlayer } from "./track-player";
 import styled from "styled-components";
-import { Track } from "./track-models";
 import { Route, Switch } from "wouter";
-import { TrackLocalStorageService } from "./common-components/TrackLocalStorageService";
-import { IncludedDataMap } from "./included-data";
-import { Panel } from "./common-components/Panel";
 import { OlTest } from "./OlTest";
 import { gitCommitSha } from "./build-info";
 import { OsmTest } from "./osm-input/OsmTest";
@@ -27,7 +22,6 @@ function App() {
           <Route path="/osm-test">
             <OsmTest />
           </Route>
-          <Route path="/track/:id">{(params) => <ViewTrackPage trackUuid={params.id} />}</Route>
           <Route path="/trackv2/:id">{(params) => <ViewTrackPageV2 trackUuid={params.id} />}</Route>
           <Route path="/:rest*">{(params) => <Error404Page path={params.rest} />}</Route>
         </Switch>
@@ -45,27 +39,6 @@ function App() {
   );
 }
 
-const ViewTrackPage: FC<{ trackUuid: string }> = ({ trackUuid }) => {
-  const track = loadTrackFromStorage(trackUuid);
-
-  if (track === null)
-    return (
-      <>
-        <Panel style={{ color: "#FF8888" }}>Track with ID {trackUuid} not found.</Panel>
-        <StartPage />
-      </>
-    );
-
-  return (
-    <>
-      <TrackPlayer initialTrack={track} />
-      <ReturnLinkContainer>
-        <ReturnLink href="/">Return to start page</ReturnLink>
-      </ReturnLinkContainer>
-    </>
-  );
-};
-
 const Error404Page: FC<{ path: string | undefined }> = ({ path }) => (
   <div>
     <h2>
@@ -76,12 +49,6 @@ const Error404Page: FC<{ path: string | undefined }> = ({ path }) => (
     </p>
   </div>
 );
-
-function loadTrackFromStorage(trackUuid: string): Track | null {
-  const trackFromStorage = TrackLocalStorageService.load(trackUuid);
-  if (trackFromStorage !== null) return trackFromStorage;
-  return IncludedDataMap.get(trackUuid) || null;
-}
 
 const MainCenterer = styled.div`
   /* display: flex; */
